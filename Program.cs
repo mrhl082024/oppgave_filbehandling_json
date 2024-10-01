73764548e709ddacdfd7bc0ef083b830d574a86a
mrhl082024 <mhrl082024@gmail.com>
2024-10-01 09:33:33 +0200

    first

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace oppgave_filbehandling_json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string path = "whatIsThis.json";
+        if (!File.Exists(path))
+        {
+            File.CreateText(path);
+        }
+
+    }
+}
